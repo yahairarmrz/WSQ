6a7be5aed4b081b9e50e5821ae61c28b3611ded3
yahairarmrz <a01229781@itesm.mx>
2015-05-04 10:44:49 -0500

    MASTERIES

---
+++ mastery25.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+ string mensaje;
+ mensaje="Hola mundo ;D";
+ cout<<mensaje<<endl;
+}
---
+++ mastery26.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+  int matriz[5][5], a=1;
+
+  for(int i=0; i<5; i++){
+    for(int j=0; j<5; j++){
+    cout<<a;
+    a++;
+  }
+  cout<<endl;
+  a=1;
+  }
+
+
+}
